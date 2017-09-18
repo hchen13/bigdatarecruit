72892658fd8fc136ae8bb442aa5360d7654e8b33
wuxiaokun <952718180@qq.com>
2017-09-18 14:22:50 +0800

    增加自动判断系统环境，寻找DIRVER

--- SpiderMan/RecruitSpider/tools/seleniumTest.py
+++ SpiderMan/RecruitSpider/tools/seleniumTest.py
@@ -2,16 +2,19 @@ from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from scrapy.selector import Selector
 import time
+import platform
+import os
+
 
 def lagouLogin(type = 'str'):
-    # 火狐浏览器
-    # browser = webdriver.Firefox('/usr/local/Cellar/geckodriver/0.18.0/bin/')
+
+    driver_path = platformJudge()
 
     # 谷歌浏览器
     chrome_opt = webdriver.ChromeOptions()
     prefs = {"profile.managed_default_content_sttings.images":2}
     chrome_opt.add_experimental_option("prefs",prefs)
-    browser = webdriver.Chrome('/Users/monstar/Downloads/chromedriver',chrome_options=chrome_opt)
+    browser = webdriver.Chrome(driver_path,chrome_options=chrome_opt)
     login_url = "https://passport.lagou.com/login/login.html?service=https%3a%2f%2fwww.lagou.com%2f"
     browser.get(login_url)
     elem_username = browser.find_element_by_xpath("//form[@class='active']/div[@data-propertyname='username']/input")
@@ -35,3 +38,13 @@ def lagouLogin(type = 'str'):
     browser.close()
     cookies = cookies_str if type == "str" else cookies_dict
     return cookies,browser
+
+def platformJudge():
+    file_dir_path = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+    com_platform = platform.uname().system
+    if com_platform == "Windows":
+        driver_name = "chromedriver.exe"
+    elif com_platform == "Darwin":
+        driver_name = "chromedriver"
+    file_path = os.path.join(file_dir_path, 'seleniumDriver', driver_name)
+    return file_path
